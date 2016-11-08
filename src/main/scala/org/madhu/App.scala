71818cb5b8bddfa5ff9153d9d6fa22b2f1ff0e6f
Madhusudanan Kandasamy <madhusudanan@in.ibm.com>
2016-11-08 19:23:05 +0530

    parquet data creation

--- src/main/scala/org/madhu/App.scala
+++ src/main/scala/org/madhu/App.scala
@@ -2,29 +2,47 @@ package org.madhu
 
 import org.apache.spark._
 import org.apache.spark.SparkContext._
+import org.apache.spark.network.protocol.Encoders.Strings
+import org.apache.spark.sql.catalyst.util.StringUtils
+import org.apache.spark.sql.{DataFrame, SaveMode}
 
 /**
  * @author ${user.name}
  */
 object App {
   
-   case class customer(id : Int, name : String, age : Int, other : String)
+   case class customer(intType : Int, longType : Long, doubleType : Double, filler : String)
   
   def main(args : Array[String]) {
     val conf = new SparkConf().setMaster("local").setAppName("test")
     val sc = new SparkContext(conf)
-
-    val ss = org.apache.spark.sql.SparkSession.builder.getOrCreate(); 
+    println(args.length)
+    val ss = org.apache.spark.sql.SparkSession.builder.getOrCreate();
     import ss.implicits._
 
-    val df = sc.parallelize(1 to 10).map(x => customer(x,"name"+x, (x/10) * 10,"junk" +x)).toDF()
+    if(args.length == 0) {
+      println("app write snappy/zlib/none sizeInMB")
+      println("app read")
+      return
+    }
 
-    df.write.parquet("customer.parquet")
+    println(Int.MaxValue)
 
-    val newdf = ss.read.parquet("customer.parquet")
+    val filler = "Filler".padTo(999,"#").mkString
 
-    newdf.show();
+    var df : DataFrame = null.asInstanceOf[DataFrame]
 
+    if(args(0).equalsIgnoreCase("write")) {
+      val length = args(2).toInt * 1024;
+      df = sc.parallelize(1 to length).map(x => customer(x,x*10, x*100,x + filler)).toDF()
+      df.write.option("compression",args(1)).mode(SaveMode.Overwrite).parquet("customer.parquet")
+      df.show()
+    }
+    else {
+      val newdf = ss.read.parquet("customer.parquet")
+      println("Total records " + newdf.collect().length);
+      newdf.show();
+    }
 
   }
 
